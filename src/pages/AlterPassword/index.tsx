ca98e7e0acae5b54b098a29cbeaa28b1a9fd7aaf
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-09-18 15:28:27 -0300

    Adiçao da rota de recuperaçao de senha

--- src/pages/AlterPassword/index.tsx
+++ src/pages/AlterPassword/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-
+import React, { useRef, useCallback, useState } from 'react';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,23 +14,85 @@ import ButtonBack from '../../components/ButtonBack';
 
 import { Container, Content, Header } from './style';
 
+import api from '../../services/api';
+
+interface AlterPasswordFormData {
+  email: string;
+}
+
 const AlterPassword: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const [loading, setLoading] = useState(false);
+
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: AlterPasswordFormData) => {
+      try {
+        setLoading(true);
+
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/password/forgot', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'E-mail de recuperação enviado',
+          description:
+            'Enviamos um e-mail para confirmar a recuperação de senha, cheque sua caixa de entrada.',
+        });
+
+        history.push('/signin');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na recuperação de senha',
+          description:
+            'Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.',
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast, history],
+  );
 
   return (
     <Container>
       <Header>
-        <ButtonBack type="submit">Voltar</ButtonBack>
+        <Link to="/signin">
+          <ButtonBack type="submit">Voltar</ButtonBack>
+        </Link>
       </Header>
       <Content>
         <Logo />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <Input name="email" placeholder="E-mail" />
 
-          <Button type="submit">Próximo</Button>
+          <Button loading={loading} type="submit">
+            Recuperar
+          </Button>
         </Form>
       </Content>
     </Container>
