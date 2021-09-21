8d9b993033848ffb54b3fa71549c29da80bb6304
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-09-20 21:26:49 -0300

    Revert "Resoluçao de conflitos"
    This reverts commit 38eec1d28a7d0b7b8693ee226f6e41a118047ee8, reversing
    changes made to 893deedf80ab26948194fe022bb97d97e633613c.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+export default createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
 :root{
   --background: #ffffff;
   --blue: #5E9DBC; // cor dos botões
@@ -12,16 +19,30 @@ export const GlobalStyle = createGlobalStyle`
   --text: #383838; // cor padrão dos textos
 }
 
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+body{
+  background: var(--background);
+  -webkit-font-smoothing: antialiased;
+  color: var(---text);
+
 }
 
-body{
-  background: var(---background);
-  color: var(---text);
-  font: 400 16px "Nunito", sans-serif;
+body, input{
+  font-family: 'Nunito', sans-serif;
+  font-size: 16px;
+}
+
+button{
+  font-family: 'Nunito', sans-serif;
+}
+
+}
+
+h1,h2,h3,h4,h4,h6,strong{
+  font-weight: 600;
+}
+
+button{
+  cursor: pointer;
 }
 
 
